f57642ed25ac9612598602fd903061f17a1287eb
Eli-michaud <47183913+Eli-michaud@users.noreply.github.com>
2021-10-27 16:26:42 -0400

    interview

--- src/Interview.java
+++ src/Interview.java
@@ -1,8 +1,16 @@
+import com.sun.source.tree.Tree;
+
 public class Interview {
 
     //TODO: completer
     public static TreeNode solution (TreeNode t1, TreeNode t2) {
-        return null;
+        if(t1 == null) return t2;
+        if(t2 == null) return t1;
+        int resultTreeVal = Math.min(t1.val, t2.val);
+        TreeNode resultTree = new TreeNode(resultTreeVal);
+        resultTree.left = solution(t1.left, t2.left);
+        resultTree.right = solution(t1.right, t2.right);
+        return resultTree;
     }
 }
 
